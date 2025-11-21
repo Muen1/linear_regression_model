7949c64ae59c7583c8c21ad0b6522efb7eae5db1
Muen1 <c.mutie@gmail.com>
2025-11-21 10:16:58 +0200

    add flutter services and ui

--- summative/FlutterApp/lib/core/api_constants.dart
+++ summative/FlutterApp/lib/core/api_constants.dart
@@ -1,5 +1,6 @@
 class ApiConstants {
   // ignore: constant_identifier_names
-  static const String PREDICT_URL = "http://192.168.1.72:8000/predict";
+  static const String PREDICT_URL =
+      "https://linear-regression-model-4dyx.onrender.com/predict";
 }
 
---
+++ summative/FlutterApp/lib/services/api_service.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutterapp/core/api_constants.dart';
+
+class ApiService {
+  // Send crop input data to the FastAPI backend
+  Future<double> predictCropYield(Map<String, dynamic> data) async {
+    try {
+      final response = await http.post(
+        Uri.parse(ApiConstants.PREDICT_URL),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(data),
+      );
+
+      if (response.statusCode == 200) {
+        final decoded = jsonDecode(response.body);
+        return decoded["Predicted_Yield"] * 1.0; // return as double
+      } else {
+        throw Exception(
+            "Server error ${response.statusCode}: ${response.body}");
+      }
+    } catch (e) {
+      throw Exception("Failed to connect to API: $e");
+    }
+  }
+}
+
